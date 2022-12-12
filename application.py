2ab41081798c8903cc6e5662c3017c8be022b01e
Tushar <19btcse61@suiit.ac.in>
2022-12-12 14:37:01 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,45 @@
+
+# importing the necessary dependencies
+from flask import Flask, render_template, request
+from flask_cors import cross_origin
+import pickle
+
+application = Flask(__name__) # initializing a flask app
+# app=application
+@application.route('/',methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+@application.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+            #  reading the inputs given by the user
+            N=float(request.form['N'])
+            P = float(request.form['P'])
+            K = float(request.form['K'])
+            temperature = float(request.form['temperature'])
+            humidity = float(request.form['humidity'])
+            ph = float(request.form['ph'])
+            rainfall = float(request.form['rainfall'])
+            filename = 'crop_rec.pickle'
+            loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
+            # predictions using the loaded model file
+            prediction=loaded_model.predict([[N,P,K,temperature,humidity,ph,rainfall]])
+            print('prediction is', prediction)
+            # showing the prediction results in a UI
+            return render_template('results.html',prediction=round(100*prediction[0]))
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    # return render_template('results.html')
+    else:
+        return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    #app.run(host='127.0.0.1', port=8001, debug=True)
+	application.run(debug=True) # running the app
